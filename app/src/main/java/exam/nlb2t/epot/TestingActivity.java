36a6d696e4f600abd4b314e4217289864610cb91
Dragnell <bang2001vl@outlook.com.vn>
2021-03-19 10:11:45 +0700

    Add TestingActivity All test should be perform in TestingActivity Change default startup activity to use MainActivity as launcher

---
+++ app/src/main/java/exam/nlb2t/epot/TestingActivity.java
@@ -0,0 +1,14 @@
+package exam.nlb2t.epot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TestingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_testing);
+    }
+}
