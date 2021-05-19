4779338c7bd345e3fedb810865b7d0af32cb0d07
UIT.19520266 <19520266@gm.uit.edu.vn>
2021-05-20 01:25:21 +0700

    complete view 1,2 in cart_view

--- app/src/main/java/exam/nlb2t/epot/Views/ItemCard_View_Thach.java
+++ app/src/main/java/exam/nlb2t/epot/Views/ItemCard_View_Thach.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -17,35 +19,37 @@ public class ItemCard_View_Thach extends BaseCustomViewGroup{
 
     public ItemCard_View_Thach(Context context) {
         super(context);
+        init(context, null);
     }
 
     public ItemCard_View_Thach(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init(context, attrs);
     }
 
     public ItemCard_View_Thach(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(context, attrs);
     }
 
     public ItemCard_View_Thach(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        init(context, attrs);
     }
 
     TextView productName;
     TextView productQuantity;
-    Button btn_decrease;
-    Button btn_increase;
+    ImageButton btn_decrease;
+    ImageButton btn_increase;
     TextView productPrice;
     ImageView img_product;
     CheckBox ckb_choose;
-
-    int step = 1;
 
     public void init(Context context, AttributeSet attrs) {
         LayoutInflater inflater = LayoutInflater.from(context);
         View view = inflater.inflate(R.layout.itemcard_template, this, false);
 
-        productName = view.findViewById(R.id.name_Product);
+        productName = view.findViewById(R.id.titlename_Product);
         productQuantity = view.findViewById(R.id.quantity_Product);
         btn_decrease = view.findViewById(R.id.btn_decrease_product);
         btn_increase = view.findViewById(R.id.btn_increase_product);
@@ -65,27 +69,50 @@ public class ItemCard_View_Thach extends BaseCustomViewGroup{
             int ItemQuantity = typedArray.getInt(R.styleable.ItemCard_View_ItemQuantity, 0);
             boolean isCheck = typedArray.getBoolean(R.styleable.ItemCard_View_ItemIsCheck, false);
 
+            float ImgSize = typedArray.getDimension(R.styleable.ItemCard_View_ItemImgSize, getResources().getDimension(R.dimen.ProductImgSize));
+            float TextSize = typedArray.getDimension(R.styleable.ItemCard_View_ItemTextSize, getResources().getDimension(R.dimen.ProductTextSize)/getResources().getDisplayMetrics().density);
+
 //            Maybe some params here
-            setData(ItemQuantity, isCheck);
+            setData(ItemQuantity, isCheck, ImgSize, TextSize);
             typedArray.recycle();
         }
 
         initListener();
     }
 
-    private void setData(int quantity, boolean isCheck) {
+    private void setData(int quantity, boolean isCheck, float imgsize, float textsize) {
 //
 //        Add data of product
 //
         ckb_choose.setChecked(isCheck);
-        productQuantity.setText(quantity);
+        productQuantity.setText(String.valueOf(quantity));
+
+        productName.setTextSize((int)textsize);
+        productQuantity.setTextSize((int)textsize);
+        productPrice.setTextSize((int)textsize);
+
+        img_product.getLayoutParams().height=(int)imgsize;
+        img_product.getLayoutParams().width=(int)imgsize;
+        img_product.requestLayout();
     }
 
     private void initListener() {
         btn_increase.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                onProductChangeListener.OnQuantityProductChanged(v, step);
+                onProductChangeListener.OnQuantityProductChanged(ItemCard_View_Thach.this, 1);
+            }
+        });
+        btn_decrease.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onProductChangeListener.OnQuantityProductChanged(ItemCard_View_Thach.this, -1);
+            }
+        });
+        ckb_choose.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                onProductChangeListener.OnCheckProductChanged(ItemCard_View_Thach.this, isChecked);
             }
         });
     }
@@ -97,12 +124,12 @@ public class ItemCard_View_Thach extends BaseCustomViewGroup{
         return ckb_choose.isChecked();
     }
 
-    public interface OnItemChangeListener {
+    public interface OnProductChangeListener {
         void OnCheckProductChanged(View view, boolean isCheck);
-        void OnQuantityProductChanged(View view, int quantity);
+        void OnQuantityProductChanged(View view, int increasement);
     }
 
-    OnItemChangeListener onProductChangeListener = new OnItemChangeListener() {
+    OnProductChangeListener onProductChangeListener = new OnProductChangeListener() {
         @Override
         public void OnCheckProductChanged(View view, boolean isCheck) {
 
@@ -114,7 +141,7 @@ public class ItemCard_View_Thach extends BaseCustomViewGroup{
         }
     };
 
-    public void setOnProductChangeListener(OnItemChangeListener listener) {
+    public void setOnProductChangeListener(OnProductChangeListener listener) {
         onProductChangeListener = listener;
     }
 }
