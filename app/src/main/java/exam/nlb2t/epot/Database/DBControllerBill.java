680cd18d49694d489c31e5a1749464cfce4b54ee
UIT.19520266 <19520266@gm.uit.edu.vn>
2021-06-17 11:59:52 +0700

    create BillBaseDB
    - Build method getBill by Status
    - Create enum BillStatus

--- app/src/main/java/exam/nlb2t/epot/Database/DBControllerBill.java
+++ app/src/main/java/exam/nlb2t/epot/Database/DBControllerBill.java
@@ -3,6 +3,7 @@ package exam.nlb2t.epot.Database;
 import android.graphics.Bitmap;
 import android.util.Log;
 
+import androidx.annotation.Nullable;
 import androidx.core.util.Pair;
 
 import java.io.ByteArrayInputStream;
@@ -12,8 +13,11 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import exam.nlb2t.epot.Database.Tables.BillBaseDB;
 
 public class DBControllerBill extends  DatabaseController{
     public boolean addBill(int userID, String address, List<Pair<Integer, Integer>> list)
@@ -58,17 +62,71 @@ public class DBControllerBill extends  DatabaseController{
         }
         return rs;
     }
-    public int getBillSuccess(int UserID) {
-        //TODO: InsertCodeHere
-    }
-    public int getBillShip(int UserID) {
-        //TODO: InsertCodeHere
-    }
-    public int getBillConfirm(int UserID) {
-        //TODO: InsertCodeHere
-    }
-    public int getBillCancel(int UserID) {
-        //TODO: InsertCodeHere
+    public int getNumberBillbyStatus(int userID, BillBaseDB.BillStatus statusBill) {
+        int number = 0;
+        try {
+            String sql = "SELECT COUNT(*) FROM [BILL] WHERE [USER_ID] = ? and [STATUS] = ?";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, userID);
+            statement.setInt(2, statusBill.getValue());
+
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                number = resultSet.getInt(1);
+            }
+            resultSet.close();
+            statement.close();
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+            return -1;
+        }
+        return number;
     }
 
+    /**
+     * Get Bills overview without detailBill by user id and status of the bill
+     * @param userID The id of the user want to get
+     * @param statusBill The status of bills want to get, it is nullable variable. The null is determine when want get all bill of user
+     * @return List bill overview
+     */
+    public List<BillBaseDB> getBillsOverviewbyStatus(int userID, @Nullable BillBaseDB.BillStatus statusBill) {
+        List<BillBaseDB> rs = new ArrayList<>();
+        try {
+            PreparedStatement statement = connection.prepareStatement("EXEC selectBillsByStatus ?,?");
+            statement.setInt(1, userID);
+            if (statusBill != null) {
+                statement.setInt(2, statusBill.getValue());
+            }
+            else {
+                statement.setNull(2, Types.INTEGER);
+            }
+
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                BillBaseDB bill = new BillBaseDB();
+
+                bill.userID = userID;
+                bill.id = resultSet.getInt("ID");
+                bill.address = resultSet.getString("ADDRESS");
+                bill.createdDate = resultSet.getDate("CREATED_DATE");
+                bill.keyBill = resultSet.getString("KEYBILL");
+
+                //if not null, return current status, else return status in the table
+                if (statusBill != null) bill.status = statusBill;
+                else bill.status = BillBaseDB.BillStatus.values()[resultSet.getInt("STATUS")];
+
+                rs.add(bill);
+            }
+
+            resultSet.close();
+            statement.close();
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return rs;
+    }
 }
---
+++ app/src/main/java/exam/nlb2t/epot/Database/Tables/BillBaseDB.java
@@ -0,0 +1,46 @@
+package exam.nlb2t.epot.Database.Tables;
+
+import java.util.Date;
+import java.util.List;
+
+import exam.nlb2t.epot.Database.DBControllerBill;
+
+public class BillBaseDB {
+    public enum BillStatus {
+        DEFAULT(1), //MEANS: Default
+        WAIT_CONFIRM(2), //MEANS: Success payment but not confirm from saler
+        IN_SHIPPING(3), //MEANS: Success confirm but buyer not give product
+        SUCCESS(4); //MEANS: Success Deal
+
+        private final int value;
+
+        private BillStatus(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
+    public int id; //[BILL].[ID]
+    public int userID; //[BILL].[USER_ID]
+    public Date createdDate; //[BILL].[CREATED_DATE]
+    public String keyBill; //[BILL].[KEYBILL]
+    public String address; //[BILL].[ADDRESS]
+    public BillStatus status; //[BILL].[STATUS]
+    public List<ProductBaseDB> productinBill;
+
+    public BillBaseDB() {
+
+    }
+
+    public static int getNumberBill(int userID, BillStatus status) {
+        DBControllerBill db = new DBControllerBill();
+        int number = db.getNumberBillbyStatus(userID, status);
+
+        db.getBillsOverviewbyStatus(1, null);
+        db.closeConnection();
+        return number;
+    }
+}
